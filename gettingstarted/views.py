44854a4d35937044a27af517d86bc8af1af12bec
Bryan Beecham <bryan_beecham@hotmail.com>
2018-05-08 09:58:32 -0400

    reset view

--- gettingstarted/views.py
+++ gettingstarted/views.py
@@ -1,11 +1,10 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello from Python!')
-    # return render(request, 'index.html')
+    # return HttpResponse('Hello from Python!')
+    return render(request, 'index.html')
 
 
 def login(request):
