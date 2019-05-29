cf865d6d3fa4285e960e540b139b059c222d4041
Bryan Beecham <bryan_beecham@hotmail.com>
2019-05-29 10:22:37 -0400

    local server now working AND static images showing

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -46,16 +46,13 @@ INSTALLED_APPS = (
     'gettingstarted'
 )
 
-# figure out what is going on here
-# should read MIDDLEWARE = ( with Django 2.2
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE = (
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
